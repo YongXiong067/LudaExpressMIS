5bc819454265683b3062c5bc7019f8e0e5e3d9c6
YongXiong067 <58913196+YongXiong067@users.noreply.github.com>
2019-12-16 20:46:42 +0800

    创建项目

---
+++ 2.Code/LudaExpressMIS/src/main/java/com/express/web/LudaExpressMisApplication.java
@@ -0,0 +1,13 @@
+package com.express.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LudaExpressMisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LudaExpressMisApplication.class, args);
+	}
+
+}
---
+++ 2.Code/LudaExpressMIS/src/test/java/com/express/web/LudaExpressMisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.express.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LudaExpressMisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
