7c613915826498e98a5fd7bf96012e91dc2c82f6
yhj <huanjin.yuan@foxmail.com>
2019-12-16 19:34:30 +0800

    创建项目

---
+++ 2.Code/LudaExpressMIS/src/test/java/com/express/LudaExpressMisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.express;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LudaExpressMisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
