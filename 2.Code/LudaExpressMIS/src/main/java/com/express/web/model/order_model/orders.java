380df950bf58220392ad1a82a821088a803bca7e
yihuihe-mine <1510315326+yihuihe-mine@users.noreply.github.com>
2019-12-18 22:17:49 +0800

    添加备注，和userid

--- 2.Code/LudaExpressMIS/src/main/java/com/express/web/model/order_model/orders.java
+++ 2.Code/LudaExpressMIS/src/main/java/com/express/web/model/order_model/orders.java
@@ -8,30 +8,33 @@ import java.util.Date;
  * @Description: 订单表
  */
 public class orders {
+    //订单id
     private Long orderid;
-
+    //关联用户表id
+    private Long userid;
+    //关联网点id
     private Long dotid;
-
+    //发件人手机号码
     private String sendtel;
-
+    //发件人地址
     private String sendaddr;
-
+    //下单时间
     private Date orderdate;
-
+    //收件人手机号码
     private String receivetel;
-
+    //收件人地址
     private String receiveaddr;
-
+    //订单编号，系统自动生成
     private Integer ordernum;
-
+    //包裹重量
     private String weight;
-
+    //订单所产生的费用
     private Double money;
-
+    //订单状态  0为预约，1为运输中，2为派送中，3为已签收
     private String state;
-
+    //当前处理人
     private String currentuser;
-
+    //处理步骤记录
     private String content;
 
     public Long getOrderid() {
@@ -136,5 +139,14 @@ public class orders {
 
     public void setContent(String content) {
         this.content = content == null ? null : content.trim();
+
+    }
+
+    public Long getUserid() {
+        return userid;
+    }
+
+    public void setUserid(Long userid) {
+        this.userid = userid;
     }
 }
