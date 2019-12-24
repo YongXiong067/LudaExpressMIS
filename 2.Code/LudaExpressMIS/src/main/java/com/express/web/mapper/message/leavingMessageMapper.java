7866f433c3c8e500c32a203f2b55386839a2678b
yihuihe-mine <1510315326+yihuihe-mine@users.noreply.github.com>
2019-12-24 16:17:23 +0800

    留言

---
+++ 2.Code/LudaExpressMIS/src/main/java/com/express/web/mapper/message/leavingMessageMapper.java
@@ -0,0 +1,63 @@
+package com.express.web.mapper.message;
+
+import com.express.web.model.message.leavingMessage;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+/**
+ * @Author: YIHUI HE
+ * @Date: 2019/12/22
+ * @Description:
+ */
+@Mapper
+public interface leavingMessageMapper {
+
+    /**
+     * sql参数
+     */
+    final static String param = "*";
+    /**
+     * 获取全部数据
+     * @param search
+     * @return
+     */
+    @Select("<script>"
+            + "select "+ param +" from leaving_message "
+            + "where 1=1 "
+            +"<if test= 'search != null'> and ( Content like concat('%',#{search},'%')) </if>"
+            +"</script>")
+    @Results(id="MessageMap",
+            value={
+                    @Result(id=true,property="leavingId",column="leavingId"),
+                    @Result(property="userId",column="userId"),
+                    @Result(property="state",column="State"),
+                    @Result(property="content",column="Content"),
+                    @Result(property="rsContert",column="resultContent")
+            }
+    )
+    List<leavingMessage> listByAll(@Param("search") String search);
+
+    /**
+     * 插入记录
+     * @param companyModel
+     */
+    @Insert("insert into leaving_message(userId, State,Content,resultContent) value(#{userId}, #{state},#{content},#{rsContert})")
+    void insert(leavingMessage leavingMessage);
+
+    /**
+     * 修改信息
+     * @return
+     */
+    @Update("update leaving_message set resultContent = #{rsContert},State = #{state}"
+            + " where leavingId = #{leavingId}")
+    boolean update(leavingMessage leavingMessage);
+
+    /**
+     * 删除信息
+     * @param id
+     * @return
+     */
+    @Delete("DELETE from leaving_message where leavingId = #{leavingId}")
+    boolean deleteById(long ncId);
+}
